9d26c79b1dce294e75f2a8bbacbe317f7c9db7c4
Thomas Rega <thomas.rega@safetyio.com>
2020-10-25 20:58:22 +0100

    Add first simple fields to enter work times

--- wtt/time_tracker/admin.py
+++ wtt/time_tracker/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from time_tracker.models import Project, Entry
 
-# Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    model = Project
+
+
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ('project', 'start_at', 'end_at', 'duration_in_minutes')
+    model = Entry
+
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Project, ProjectAdmin)
+
--- wtt/time_tracker/models.py
+++ wtt/time_tracker/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    """Project you worked for."""
+    name = models.CharField(max_length=32, primary_key=True)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
+
+class Entry(models.Model):
+    """Single entry for work time."""
+    project = models.ForeignKey(Project, on_delete=models.PROTECT)
+    start_at = models.DateTimeField(null=True, blank=True)
+    end_at = models.DateTimeField(null=True, blank=True)
+    duration_in_minutes = models.IntegerField(
+        "Duration in Minutes", null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        """Calculate duration on save."""
+        if self.end_at:
+            time_delta = self.end_at - self.start_at
+            total_seconds = time_delta.total_seconds()
+            self.duration_in_minutes = total_seconds / 60
+
+        super().save(*args, **kwargs)
